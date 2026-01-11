a014be9250c932439991db02d6e714782142d1db
staylor2013 <staylor.26@wymcol.org>
2026-01-11 16:34:51 +0000

    Update Bashuno.js

--- Bashuno.js
+++ Bashuno.js
@@ -1,1 +1,15 @@
+const readline = require('readline');
+const colors = ['red', 'yellow', 'blue', 'green'];
+const numbers = 
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to UNO! Press Enter to start...', () => {
+  console.log('Game starting...');
+  rl.close();
+  createdeck();
+});
 
